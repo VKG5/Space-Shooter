ede7c2538813d947b187926625d3dc1a81d4ec76
Lumi_V <bawagupta69@gmail.com>
2020-07-25 22:14:24 +0530

    Spawing Enemies Automatically

---
+++ Assets/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveconfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveconfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.getNumberOfEnemies(); i++)
+        {
+            Instantiate(waveConfig.getEnemyPrefab(),
+                waveConfig.getWaypoints()[0].transform.position,
+                Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.getTimeBetweenSpawns());
+        }
+    }
+}
