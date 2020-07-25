e11b87400165e142b3c4aa9f0310caf94bfba136
Lumi_V <bawagupta69@gmail.com>
2020-07-25 21:48:09 +0530

    Basic Scriptable Objects setup

--- Assets/Assets/Scripts/EnemyPathing.cs
+++ Assets/Assets/Scripts/EnemyPathing.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveconfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 3f;
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        waypoints = waveconfig.getWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
 
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards
+                (transform.position,
+                    targetPosition,
+                    movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
